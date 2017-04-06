3ef523c4e98f5dc4b7bbdbf9470454645863faf5
pvazteixeira <pvt@mit.edu>
2017-04-06 16:14:35 -0400

    working towards minimal version.
    - added pcl support
    - added pose2d and pose3d classes

--- src/rome/rome.cpp
+++ src/rome/rome.cpp
@@ -2,25 +2,58 @@
 
 namespace RoME {
 
-  void RoME::AddPose(const Eigen::Vector3d &position,
-                     const Eigen::Quaterniond &orientation, const int pose_id) const {
-    lcm::LCM lcm_node();
-    rome::pose_node_t pose_message;
+void RoME::AddPose(const Pose3d &pose, const int pose_id) const {
+  rome::pose_node_t pose_message;
 
-  }
+  pose_message.utime = 0;
+  pose_message.id = pose_id;
+  pose_message.mean_dim = 7;
+  std::vector<double> pose_mean = {pose.x(),  pose.y(),  pose.z(), pose.qw(),
+                                   pose.qx(), pose.qy(), pose.qz()};
+  pose_message.mean = pose_mean;
+  pose_message.covar_dim = 6;
+  std::vector<double> covar_diag = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+  pose_message.covar = covar_diag;
 
-  void RoME::AddPartialXYH(const Eigen::Vector3d &rel_pose,
-                           const Eigen::Matrix3d &Sigma, const int origin_id,
-                           const int dest_id, const double confidence ) const {}; 
-
-  void RoME::AddPriorXYH(const Eigen::Vector3d &pose, const Eigen::Matrix3d &Sigma,
-                         const int pose_id) const {};
-
-  // this method alone makes this a Caesar client instead of just RoME.
-  // void RoME::AddCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
-  //                     const int pose_id) const {};
-
+  lcm::LCM lcm_node;
+  lcm_node.publish("ROME_POSES", &pose_message);
 }
 
+void RoME::AddPartialXYH(const Pose2d &rel_pose, const int origin_id,
+                         const int dest_id) const {
+    // rename AddPartial2D(...)?
+};
 
+void RoME::AddPriorZPR(double z, double pitch, double roll,
+                       const Eigen::Matrix3d &Sigma, const int pose_id) const {
+    // adds a prior in Z, pitch, and roll
+};
 
+void RoME::AddPose3Pose3NH(const Eigen::Vector3d &rel_position,
+                           const Eigen::Quaterniond &rel_orientation,
+                           const Eigen::MatrixXd &Sigma, const int origin_id,
+                           const int dest_id) const {
+    // will be implemented
+};
+
+void RoME::AddCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
+                    const int pose_id) const {
+  rome::point_cloud_t point_cloud_message;
+
+  point_cloud_message.utime = 0;
+  point_cloud_message.id = pose_id;
+
+  point_cloud_message.n = cloud->points.size();
+  for (int i = 0; i < cloud->points.size(); ++i) {
+    pcl::PointXYZRGB pt = cloud->points[i];
+  std:
+    std::vector<double> ptv = {pt.x, pt.y, pt.z};
+    std::vector<uint8_t> ptc = {pt.r, pt.g, pt.b};
+    point_cloud_message.points.push_back(ptv);
+    point_cloud_message.colors.push_back(ptc);
+  }
+
+  lcm::LCM lcm_node;
+  lcm_node.publish("ROME_POINT_CLOUDS", &point_cloud_message);
+};
+}
--- src/rome/rome.hpp
+++ src/rome/rome.hpp
@@ -3,21 +3,39 @@
 #include <Eigen/Dense>
 
 #include <lcm/lcm-cpp.hpp>
+
+#include <pcl/common/common.h>
+#include <pcl/common/common_headers.h>
+
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+
+#include <pcl/point_types.h>
+
+#include "Pose3d.hpp"
 #include <lcmtypes/rome.hpp>
-
-// #futureinclude neo4j
-
-// #include <pcl/common/common_headers.h>
-// #include <pcl/io/pcd_io.h>
-// #include <pcl/point_types.h>
-
 // let's try and keep this stateless, yes?
 // although it would be nice to have a way to get pose updates.
 // look at caesar/examples/database/python/neo4j_interact.py for API ideas
+// [[file+emacs:~/.julia/v0.5/Caesar/examples/database/python/neo4j_interact.py][neo4j_interact.py]]
 
 namespace RoME {
 
-class Pose {};
+class Pose2d {
+public:
+  Pose2d() {
+    position_ << 0.0, 0.0;
+    yaw_ = 0.0;
+  };
+  Pose2d(double x, double y, double yaw) {
+    position_ << x, y;
+    yaw_ = yaw;
+  };
+
+private:
+  Eigen::Vector2d position_;
+  double yaw_;
+};
 
 class Noise {};
 
@@ -27,19 +45,22 @@ public:
   RoME(){};
   ~RoME(){};
 
-  void AddPose(const Eigen::Vector3d &position,
-               const Eigen::Quaterniond &orientation, const int pose_id) const;
+  void AddPose(const Pose3d &pose, const int pose_id) const;
 
-  void AddPartialXYH(const Eigen::Vector3d &rel_pose,
-                     const Eigen::Matrix3d &Sigma, const int origin_id,
-                     const int dest_id, const double confidence) const;
+  void AddPartialXYH(const Pose2d &rel_pose, const int origin_id,
+                     const int dest_id) const;
 
-  void AddPriorXYH(const Eigen::Vector3d &pose, const Eigen::Matrix3d &Sigma,
-                   const int pose_id) const;
+  void AddPriorZPR(double z, double pitch, double roll,
+                   const Eigen::Matrix3d &Sigma, const int pose_id) const;
+
+  void AddPose3Pose3NH(const Eigen::Vector3d &rel_position,
+                       const Eigen::Quaterniond &rel_orientation,
+                       const Eigen::MatrixXd &Sigma, const int origin_id,
+                       const int dest_id) const;
 
   // this method alone makes this a Caesar client instead of just RoME.
-  // void AddCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
-  //               const int pose_id) const;
+  void AddCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
+                const int pose_id) const;
 
 private:
 };
