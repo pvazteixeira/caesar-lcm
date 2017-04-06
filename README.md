88254cde5fe982d031bdacd3bbd26fcc00ba881d
pvazteixeira <pvt@mit.edu>
2017-04-06 18:10:34 -0400

    Updated example.

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
-# RoME LCM types
+# RoME-LCM
 
 A C++ interface for [RoME](https://github.com/dehann/rome.jl) over [LCM](https://lcm-proj.github.io).
 
 ## Dependencies
 
-* [Eigen]()
-* [LCM]()
-* [PCL]()
+* [Eigen]() - `libeigen3-dev`
+* [LCM]() - 
+* [PCL]() - `libpcl-dev`
 
 ## Installation
 
 Run `make` from the repository root. Executables (from `src/exes`) will be placed under `build/bin`, header files under `build/include`, and shared libraries in `build/lib`. `pkg-config` files will be in `build/lib/pkgconfig`.
+
+## Examples
+
+`build/bin/test` will create two poses, `x1` and `x2`, add priors (on depth, pitch and roll), connect them with an odometry factor (on x, y, and yaw) and add a point cloud to each.
 
 ## Notes
 This software is constructed according to the Pods software policies and
--- src/exes/test/main.cpp
+++ src/exes/test/main.cpp
@@ -1,19 +1,53 @@
-#include <chrono>
-//#include <csignal>
 #include <iostream>
-#include <mutex>
-#include <queue>
-#include <string>
-#include <thread>
-#include <vector>
 
 #include <lcm/lcm-cpp.hpp>
 #include <lcmtypes/rome.hpp>
 
+#include <Eigen/Dense>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
 #include <rome/rome.hpp>
 
 int main(int argCount, char **argValues) {
-  
-  RoME::RoME rome; 
 
+  RoME::RoME rome;
+
+  std::clog << "Adding pose 1...";
+  RoME::Pose3d pose_1(19.4299, 2.29964, 3.8303, 0.38004, -0.299951, -0.561802,
+                      0.670804);
+  rome.AddPose(pose_1, 1);
+  std::clog << "done.\n";
+
+  // std::clog << "Adding ZPR prior to pose 1...";
+  // std::clog << "done.\n";
+  
+  std::clog << "Adding pose 2...";
+  RoME::Pose3d pose_2(21.806, 3.39461, 3.80669, 0.390637, -0.345967, -0.582213,
+                      0.623488);
+  rome.AddPose(pose_2, 2);
+  std::clog << "done.\n";
+
+  // std::clog << "Adding ZPR prior to pose 2...";
+  // std::clog << "done.\n";
+
+  // std::clog << "Adding XYH odometry betwen poses 1 and 2...";
+  // std::clog << "done.\n";
+
+  std::clog << "Adding cloud 1...";
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud1(
+      new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::io::loadPCDFile<pcl::PointXYZRGB>("../../data/cloud1.pcd", *cloud1);
+  rome.AddCloud(cloud1, 1);
+  std::clog << "done.\n";
+
+  std::clog << "Adding cloud 2...";
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud2(
+      new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::io::loadPCDFile<pcl::PointXYZRGB>("../../data/cloud2.pcd", *cloud2);
+  rome.AddCloud(cloud2, 2);
+  std::clog << "done.\n";
+
+  std::clog << "Exiting.\n\n";
 }
