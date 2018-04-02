4a726820ca2c3d2dfa19d256e1589d259375b2b4
pvazteixeira <pvt@mit.edu>
2018-04-02 16:58:59 -0400

    updated travis (again)

--- install-lcm.sh
+++ install-lcm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set ex
+set -ex
 wget https://github.com/lcm-proj/lcm/archive/v1.3.1.tar.gz -O lcm.tar.gz
 tar -xvf lcm.tar.gz
 cd lcm-1.3.1 && ./bootstrap.sh && ./configure && make && sudo make install
