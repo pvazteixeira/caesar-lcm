42d814aec3ef4d904bfd177147decd3d725faea0
pvazteixeira <pvt@mit.edu>
2018-04-02 16:32:29 -0400

    lcm install script

---
+++ install-lcm.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set ex
+wget https://github.com/lcm-proj/lcm/archive/v1.3.1.tar.gz -O lcm.tar.gz
+tar -xvf lcm.tar.gz
+cd lcm-1.3.1 && ./bootstrap.sh && ./configure && make && sudo make install
